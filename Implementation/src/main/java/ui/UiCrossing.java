cf42c097d16dfc3ea0f232d444317d334a8a7a4b
Svetoslav Stoyanov <426911@student.fontys.nl>
2020-12-03 12:14:09 +0100

    up classes

---
+++ Implementation/src/main/java/ui/UiCrossing.java
@@ -0,0 +1,12 @@
+package ui;
+
+import java.util.Observable;
+
+public class UiCrossing implements UiCrossingInterface {
+    UiLane uiLane;
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+}
---
+++ Implementation/src/main/java/ui/UiCrossingInterface.java
@@ -0,0 +1,5 @@
+package ui;
+
+public interface UiCrossingInterface extends Ui{
+    //methods
+}
