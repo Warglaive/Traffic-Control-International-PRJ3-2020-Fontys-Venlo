ad43c5fbde57691db90ae54baaa4f23aa4781635
Svetoslav Stoyanov <426911@student.fontys.nl>
2020-12-07 11:35:53 +0100

    srs refactor and class modified
    added some lines and a constructor

---
+++ Implementation/src/main/java/ui/UICrossing/UIFourWayCrossing.java
@@ -0,0 +1,10 @@
+package ui.UICrossing;
+
+import lane.Lane;
+
+public abstract class UIFourWayCrossing {
+    private Lane leftLane;
+    private Lane rightLane;
+    private Lane topLane;
+    private Lane bottomLane;
+}
--- Implementation/src/main/java/ui/UICrossing/UIFourWayCrossingEntrance.java
+++ Implementation/src/main/java/ui/UICrossing/UIFourWayCrossingEntrance.java
@@ -1,4 +1,21 @@
 package ui.UICrossing;
 
-public class UIFourWayCrossingEntrance {
+import lane.Lane;
+
+public class UIFourWayCrossingEntrance extends UIFourWayCrossing {
+    private Lane leftLane;
+    private Lane rightLane;
+    private Lane topLane;
+    private Lane bottomLane;
+
+    private UIFourWayCrossingEntrance(Lane leftLane, Lane rightLane, Lane topLane, Lane bottomLane) {
+        this.leftLane = leftLane;
+        this.rightLane = rightLane;
+        this.topLane = topLane;
+        this.bottomLane = bottomLane;
+    }
+
+    private void LeftCrossingLights() throws InterruptedException {
+        this.leftLane.cycleStraightLights();
+    }
 }
