903ff38dbab0367b2e9f7061286ada65ef0b0c98
Maxeye11 <60927802+Maxeye11@users.noreply.github.com>
2020-12-07 10:34:54 +0100

    Add UI Lane

--- Implementation/src/main/java/UILane/UiLane.java
+++ Implementation/src/main/java/UILane/UiLane.java
@@ -1,4 +1,18 @@
 package UILane;
 
+import UILight.UIPedestrianLight;
+import UILight.UITrafficLight;
+
 public class UiLane {
+
+    private UITrafficLight trafficLight;
+    private UIPedestrianLight pedestrianLightOne;
+    private UIPedestrianLight pedestrianLightTwo;
+
+    public UiLane(UITrafficLight tLight, UIPedestrianLight pLight){
+        this.trafficLight = tLight;
+        this.pedestrianLightOne = pLight;
+        this.pedestrianLightTwo = pLight;
+    }
+
 }
---
+++ Implementation/src/main/java/UILight/UIPedestrianLight.java
@@ -0,0 +1,4 @@
+package UILight;
+
+public class UIPedestrianLight {
+}
