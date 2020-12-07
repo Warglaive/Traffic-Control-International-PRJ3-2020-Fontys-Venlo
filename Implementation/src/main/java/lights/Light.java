fe1c8fb53f3a99c1ce9a221bdea3e2b656f58fcd
Niklas <n.thurnau@student.fontys.nl>
2020-12-07 10:19:23 +0100

    added more java doc

--- Implementation/src/main/java/lights/Light.java
+++ Implementation/src/main/java/lights/Light.java
@@ -4,13 +4,34 @@ import annotations.Generated;
 import lightBehaviours.LightBehaviour;
 import locations.Location;
 
+/**
+ * The interface Light.
+ */
 public interface Light {
+    /**
+     * Change color.
+     */
     void changeColor();
 
+    /**
+     * Sets change behaviour.
+     *
+     * @param changeBehaviour the change behaviour
+     */
     void setChangeBehaviour(LightBehaviour changeBehaviour);
 
+    /**
+     * Gets change behaviour.
+     *
+     * @return the change behaviour
+     */
     LightBehaviour getChangeBehaviour();
 
+    /**
+     * Gets location.
+     *
+     * @return the location
+     */
     Location getLocation();
 
 }
