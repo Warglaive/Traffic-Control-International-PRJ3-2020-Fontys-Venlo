e255e15f8bbf0e3227dc2a4565ffb72a94ae9787
PRenkel <60927909+PRenkel@users.noreply.github.com>
2020-12-08 11:27:58 +0100

    Retrospective, fixed compiler error

--- Implementation/src/main/java/ui/UICrossing/UIFourWayCrossing.java
+++ Implementation/src/main/java/ui/UICrossing/UIFourWayCrossing.java
@@ -4,12 +4,5 @@ import lane.Lane;
 import ui.UILane.UiLane;
 
 public abstract class UIFourWayCrossing {
-    private UiLane leftLane;
-    private UiLane rightLane;
-    private UiLane topLane;
-    private UiLane bottomLane;
 
-    public UIFourWayCrossing(Lane leftLane, Lane rightLane, Lane topLane, Lane bottomLane) {
-        this.leftLane = new UiLane(leftLane);
-    }
 }
