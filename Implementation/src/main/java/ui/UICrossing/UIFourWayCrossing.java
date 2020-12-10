08f5f0c7f4ab12cdb36162a6e44b2d6325670c6b
Svetoslav Stoyanov <426911@student.fontys.nl>
2020-12-10 12:19:29 +0100

    update

--- Implementation/src/main/java/ui/UICrossing/UIFourWayCrossing.java
+++ Implementation/src/main/java/ui/UICrossing/UIFourWayCrossing.java
@@ -3,8 +3,5 @@ package ui.UICrossing;
 import ui.UILane.UiLane;
 
 public abstract class UIFourWayCrossing implements UiCrossing {
-    private UiLane leftLane;
-    private UiLane rightLane;
-    private UiLane topLane;
-    private UiLane bottomLane;
+
 }
