e7d8ae4783acda2594a3a827affe3a97f9ff3dc5
OgOger <j.polfers@student.fontys.nl>
2020-12-10 18:58:33 +0100

    Fixed some errors

--- Implementation/src/main/java/ui/UILight/UITrafficLightStandard.java
+++ Implementation/src/main/java/ui/UILight/UITrafficLightStandard.java
@@ -1,10 +1,12 @@
 package ui.UILight;
 
 import lights.ObserverLight;
+import ui.Controller.TrafficLightRepresentation;
 
 public abstract class UITrafficLightStandard extends UITrafficLightObserver {
 
-    public UITrafficLightStandard(ObserverLight businessLight, Country country) {
-        super(businessLight, country);
+    public UITrafficLightStandard(ObserverLight businessLight, Country country, TrafficLightRepresentation trafficLightRepresentation) {
+
+        super(businessLight, country, trafficLightRepresentation);
     }
 }
