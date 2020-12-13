2d3603b3514b815ee8e2e8b7137a9307c51bce96
Maxeye11 <60927802+Maxeye11@users.noreply.github.com>
2020-12-13 16:47:12 +0100

    Create UIThreeWayCrossingEntrance

--- Implementation/src/main/java/ui/UICrossing/UIThreeWayCrossingEntrance.java
+++ Implementation/src/main/java/ui/UICrossing/UIThreeWayCrossingEntrance.java
@@ -1,4 +1,16 @@
 package ui.UICrossing;
 
+import lane.Lane;
+import ui.UILane.UiLane;
+
 public class UIThreeWayCrossingEntrance {
+    private UiLane leftLane;
+    private UiLane rightLane;
+    private UiLane bottomLane;
+
+    public UIThreeWayCrossingEntrance(Lane leftLane, Lane rightLane, Lane bottomLane) {
+        this.leftLane = new UiLane(leftLane);
+        this.rightLane = new UiLane(rightLane);
+        this.bottomLane = new UiLane(bottomLane);
+    }
 }
