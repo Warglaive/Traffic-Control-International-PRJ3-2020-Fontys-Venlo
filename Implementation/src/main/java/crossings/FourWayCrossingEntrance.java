1491497205ef7e8522f0efd1e24bfd841bc75680
PRenkel <p.renkel@student.fontys.nl>
2020-12-14 10:25:24 +0100

    Added javadoc

--- Implementation/src/main/java/crossings/FourWayCrossingEntrance.java
+++ Implementation/src/main/java/crossings/FourWayCrossingEntrance.java
@@ -9,6 +9,11 @@ import java.util.Map;
 public abstract class FourWayCrossingEntrance implements FourWayCrossing {
     private FourWayCrossingControllerEntrance controller;
 
+    /**
+     * Creates a crossing and the parts required for it
+      * @param parameterList Contains the Information required to create Lanes and lane controllers, LaneType is the position of the lane.
+     * @param secondsBetweenLaneSwitch Te seconds that it takes to start the cycle on another lanes.
+     */
     public FourWayCrossingEntrance(Map<LaneType, Map<LaneControllerType, Map<LaneParameterKey, Object>>> parameterList, int secondsBetweenLaneSwitch) {
         this.changeController(parameterList, secondsBetweenLaneSwitch);
     }
