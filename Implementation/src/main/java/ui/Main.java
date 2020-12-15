26479c23c21f11531c3f54da521b14e62f4bf66c
PRenkel <60927909+PRenkel@users.noreply.github.com>
2020-12-15 11:19:53 +0100

    Planning poker sprint 6 added from last week

--- Implementation/src/main/java/ui/Main.java
+++ Implementation/src/main/java/ui/Main.java
@@ -5,7 +5,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import lightBehaviours.LightBehaviour;
 import lightBehaviours.PedestrianLightBehaviourStandard;
